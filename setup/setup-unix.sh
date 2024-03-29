47f1c8c201e90963667760aef4e2c8b24e693bfe
Rudyrue <73214127+Rudyrue@users.noreply.github.com>
2024-03-28 18:28:30 -0700

    Update setup-unix.sh

--- setup/setup-unix.sh
+++ setup/setup-unix.sh
@@ -3,15 +3,13 @@
 #
 # REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
 # https://haxe.org/download/version/4.2.5/
-haxelib install lime
-haxelib install openfl
-haxelib install flixel
-haxelib install flixel-addons
-haxelib install flixel-ui
-haxelib install flixel-tools
-haxelib install SScript
-haxelib install hxCodec
-haxelib install tjson
+haxelib install lime 8.1.2
+haxelib install openfl 9.3.3
+haxelib install flixel 5.5.0
+haxelib install flixel-addons 3.2.2
+haxelib install flixel-ui 2.5.0
+haxelib install SScript 17.2.618
+haxelib install hxvlc
 haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
