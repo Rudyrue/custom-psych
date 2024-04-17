2a8ec745d9e0485b091cbfdc3ac2b63823ae7f12
Rudyrue <73214127+Rudyrue@users.noreply.github.com>
2024-04-16 22:04:06 -0700

    update cli files

--- setup/setup-unix.sh
+++ setup/setup-unix.sh
@@ -1,15 +1,12 @@
-#!/bin/sh
-# SETUP FOR MAC AND LINUX SYSTEMS!!!
-#
-# REMINDER THAT YOU NEED HAXE INSTALLED PRIOR TO USING THIS
-# https://haxe.org/download/version/4.2.5/
-haxelib install lime 8.1.2
-haxelib install openfl 9.3.3
+# same thing as `setup-windows.bat` but for mac and linux systems lmao
 haxelib install flixel 5.5.0
+haxelib install openfl 9.3.3
+haxelib install lime 8.1.2
 haxelib install flixel-addons 3.2.2
 haxelib install flixel-ui 2.5.0
 haxelib install SScript 17.2.618
-haxelib install hxvlc
-haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
+haxelib install hxvlc 1.3.0
+haxelib install hxcpp 4.3.2
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
-haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+haxelib git hxdiscord_rpc https://github.com/MAJIGSAW77/hxdiscord_rpc
+haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
--- setup/setup-windows.bat
+++ setup/setup-windows.bat
@@ -3,15 +3,16 @@ color 0a
 cd ..
 @echo on
 echo Installing dependencies.
-haxelib install lime 8.1.2
-haxelib install openfl 9.3.3
 haxelib install flixel 5.5.0
+haxelib install openfl 9.3.3
+haxelib install lime 8.1.2
 haxelib install flixel-addons 3.2.2
 haxelib install flixel-ui 2.5.0
 haxelib install SScript 17.2.618
-haxelib install hxvlc
-haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
+haxelib install hxvlc 1.3.0
+haxelib install hxcpp 4.3.2
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
-haxelib git hxdiscord_rpc https://github.com/MAJigsaw77/hxdiscord_rpc
+haxelib git hxdiscord_rpc https://github.com/MAJIGSAW77/hxdiscord_rpc
+haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 echo Finished!
 pause
